0a02b33b3f1c55e088bb5dbf4e7819febb1e6d6d
mayanag <mayanag@ucla.edu>
2020-01-07 16:52:51 -0800

    make it better

---
+++ frontend/soundcloud.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  //const store = configureStore();
+  const root = document.getElementById("root")
+  ReactDOM.render(<h1>hi</h1>, root)
+});
+
+
